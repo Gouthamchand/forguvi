3d6c126ac15bf412cf6f786831b43fd5c5a99a31
Gouthamchand <32129249+Gouthamchand@users.noreply.github.com>
2017-09-20 20:01:19 +0530

    Add files via upload

---
+++ pro3.java
@@ -0,0 +1,21 @@
+package guvi;
+
+import java.util.Scanner;
+
+
+public class pro3 {
+    public static void main(String[] args) {
+     
+    char ch;
+    Scanner in=new Scanner(System.in);
+    System.out.println("Enter the character:");
+    ch=in.next().charAt(0);
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+            System.out.println("The character is vowel.");
+    else
+            System.out.println("The character is consonant.");
+        
+    
+    
+   }
+}
